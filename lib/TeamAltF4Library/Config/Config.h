3ff995693ca9544fe2120b44b555fa66de6106aa
TBK187 <messi@blessen.de>
2026-06-19 22:41:54 +0200

    New version with innit Library

--- lib/TeamAltF4Library/Config/Config.h
+++ lib/TeamAltF4Library/Config/Config.h
@@ -1,22 +1,11 @@
 #pragma once
 #include <Arduino.h>
 
-// Used to get variables that will not be change while running the programm.
-// 
-// Easy acces about app->getConfig().NameOfTheVariable when the variable is used 
-// while the programm is running. If the variable is essential for the compiler, 
-// it needs to be accesed directly with Config::NameOfTheVariable.  
+ 
 class Config {
 public:
-	static constexpr int MAX_SENSORS = 20; 
-	static constexpr int MAX_ERROR_MESSAGES = 5;
+	static const int IR_SENSOR_PINS[16];
+	static const char* IR_SENSOR_NAMES[16];
+	static const float IR_SENSOR_ANGLES[16];
 
-	static constexpr int COUNT_IR_SENSOR = 16; 
-	static const int IR_SENSOR_PINS[COUNT_IR_SENSOR];
-	static const char* IR_SENSOR_NAMES[COUNT_IR_SENSOR];
-	static const float IR_SENSOR_ANGLES[COUNT_IR_SENSOR];
-
-	static constexpr int COUNT_MOVING_AVERAGE = 3;
-
-	
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,48 @@
 #include <Arduino.h>
-#include "app/Application.h"
+#include "app/ApplicationInnit.h"
 #include "config/Config.h"
 #include <tuple>
 
-Application app;
+ApplicationInnit app;
 
-IrSensor* s[Config::COUNT_IR_SENSOR];
+IrSensor* s[16];
 
-MovingAverage ballDistnaceAvrg(Config::COUNT_MOVING_AVERAGE);
-MovingAverage ballAngleAvrg(Config::COUNT_MOVING_AVERAGE,true);
+MovingAverage ballDistnaceAvrg(3);
+MovingAverage ballAngleAvrg(3,true);
 
 unsigned long sendingPeriod = 20;
 unsigned long lastTime = 0;
 
 void setup() {
-	for (int i = 0; i < Config::COUNT_IR_SENSOR; i++) {
+
+	for (int i = 0; i < 16; i++)	{
+    
+
+		app.getSensorManager().createIrSensor(Config::IR_SENSOR_PINS[i], Config::IR_SENSOR_NAMES[i], 
+									Config::IR_SENSOR_ANGLES[i]);	
+		}
+
+	app.getSerialComm().startSerialConnection(false,false);
+
+	app.getSerialComm().enableSerialComm();
+
+
+	Serial.println("setup ok");
+
+	for (int i = 0; i < 16; i++) {
 		s[i] =app.getSensorManager().getIrSensorByIndex(i);
+		app.getSerialValuePool().set("IR" + String(i), 0.0F);
 	}
 	
-	Serial1.begin(115200); // Setup the connection to the Teensy
+	//Serial1.begin(115200); // Setup the connection to the Teensy
 
 	//Serial.begin(9600);
+
+	app.getIrSensorReader().initializeIrSensorReader(16);
+	app.getBallPositionReader().initializeBallPositionReader(16);
+
+	app.getSerialValuePool().set("IR Dist" , 0.0F);
+	app.getSerialValuePool().set("IR Angle", 0.0F);
 }
 
 
@@ -35,16 +57,16 @@ void loop() {
 	app.getBallPositionReader().setValues();
 	app.getBallPositionReader().setHighestSensor();
 	
-	// === CALCULATE THE BALL VECTOR ===
+	//// === CALCULATE THE BALL VECTOR ===
 	app.getBallPositionReader().setHighestSensVec();
 	app.getBallPositionReader().addHighestSensVec();
 	Vector ballVector = app.getBallPositionReader().getBallVector();
 	
 	// === CALCULATE THE BALLS DISTANCE AND ANGLE ===
 	float angle = 0, strength = 0;
-	std::tie(angle, strength) = app.getGeometrie().vectorToAngle(ballVector); 
+	std::tie(angle, strength) = app.getGeometry().vectorToAngle(ballVector); 
 
-	angle = app.getGeometrie().radToAngle(angle);
+	angle = app.getGeometry().radToAngle(angle);
 	
 	// === GET THE MOVING AVERAGE OF THE BALLS POSITION ===
 	ballDistnaceAvrg.newValue(strength);
@@ -53,46 +75,56 @@ void loop() {
 	ballAngleAvrg.newValue(angle);
 	float newAngle = ballAngleAvrg.getAverage();
 
-	// === SEND THE BALL ANGLE AND STRENGTH TO THE TEENSY ===
-	if (millis() - lastTime > sendingPeriod) {
+	for (int i = 0; i < 16; i++) {
+		
+		app.getSerialValuePool().set("IR" + String(i), s[i]->getCalculatedValue());
+	}
 
-		//highestSensors = app.getBallPositionReader().getHighestSensors();
+	app.getSerialValuePool().set("IR Dist" , newStrength);
+	app.getSerialValuePool().set("IR Angle", newAngle);
+
+	//// === SEND THE BALL ANGLE AND STRENGTH TO THE TEENSY ===
+	//if (millis() - lastTime > sendingPeriod) {
+
+	//	//highestSensors = app.getBallPositionReader().getHighestSensors();
 		 
-		//Serial.println("___________");
-		//for (size_t i = 0; i < 5; i++) {
-		//	Serial.print(highestSensors[i]->getAngle());
-		//  Serial.print("[");
-		//	Serial.print(highestSensors[i]->getVector().getX());
-		//  Serial.print(",");
-		//	Serial.print(highestSensors[i]->getVector().getY());
-		//  Serial.println("]");
-		//}
+	//	//Serial.println("___________");
+	//	//for (size_t i = 0; i < 5; i++) {
+	//	//	Serial.print(highestSensors[i]->getAngle());
+	//	//  Serial.print("[");
+	//	//	Serial.print(highestSensors[i]->getVector().getX());
+	//	//  Serial.print(",");
+	//	//	Serial.print(highestSensors[i]->getVector().getY());
+	//	//  Serial.println("]");
+	//	//}
 
-		//Serial.print("[");
-		//Serial.print(ballVector.getX());
-		//Serial.print(",");
-		//Serial.print(ballVector.getY());
+	//	//Serial.print("[");
+	//	//Serial.print(ballVector.getX());
+	//	//Serial.print(",");
+	//	//Serial.print(ballVector.getY());
 
-		//Serial.print("] strength: ");
-  	//Serial.print(strength);
+	//	//Serial.print("] strength: ");
+  //	//Serial.print(strength);
 
-		//Serial.print(" neuer Abstand: ");
+	//	//Serial.print(" neuer Abstand: ");
 		
 
-		//Serial.print(" Winkel: ");
-  	//Serial1.print(angle);
+	//	//Serial.print(" Winkel: ");
+  //	//Serial1.print(angle);
 
-		//Serial.print(" neuer Winkel: ");
-		Serial1.print("<");
+	//	//Serial.print(" neuer Winkel: ");
+	//	Serial1.print("<");
 		
-		Serial1.print(newAngle);
-		Serial1.print(",");
-		Serial1.print(newStrength);
+	//	Serial1.print(newAngle);
+	//	Serial1.print(",");
+	//	Serial1.print(newStrength);
 
-		Serial1.print(">");
+	//	Serial1.print(">");
 
-		Serial1.flush();
-		//Serial.println(" ; ");
-		lastTime = millis();
-	}	
+	//	Serial1.flush();
+	//	//Serial.println(" ; ");
+	//	lastTime = millis();
+	//}	
+
+	app.getSerialComm().sendingLoop();
 }
